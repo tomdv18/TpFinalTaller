b1ffadbce333ab622d03ecd0021c1c63445f4e05
fource <kevincre20@gmail.com>
2024-05-27 14:21:45 -0300

    Agregado servidor.h

---
+++ src/server_src/servidor.h
@@ -0,0 +1,26 @@
+#ifndef SERVER_SERVIDOR_H_
+#define SERVER_SERVIDOR_H_
+
+#include <string>
+#include <iostream>
+#include <sys/socket.h>
+#include "../common_src/thread.h"
+#include "aceptador.h"
+
+class Servidor{
+private:
+    Socket skt_servidor;
+
+    Aceptador aceptador;
+public:
+    explicit Servidor(const char *puerto);
+
+    void run();
+
+    Servidor(const Servidor&) = delete;
+    Servidor& operator = (const Servidor&) = delete;
+
+};
+
+
+#endif
