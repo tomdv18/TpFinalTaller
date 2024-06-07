b051a4d7288c4bfbaf580d87bf043c900dc5106e
tomdv18 <tdellavecchia@fi.uba.ar>
2024-06-07 19:47:50 -0300

    Eliminado carpetas innecesarias

--- src/extern/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tom/Desktop/TPFINAL/TpFinalTaller/extern/googletest
-# Build directory: /home/tom/Desktop/TPFINAL/TpFinalTaller/src/extern/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- src/extern/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tom/Desktop/TPFINAL/TpFinalTaller/extern/googletest/googlemock
-# Build directory: /home/tom/Desktop/TPFINAL/TpFinalTaller/src/extern/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
