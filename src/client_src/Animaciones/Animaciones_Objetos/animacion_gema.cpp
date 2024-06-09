93b2ded32addec86c59009c688d6b83091416fbd
lTomaas <psilva@fi.uba.ar>
2024-06-09 19:45:14 -0300

    Corregido animacion objetos

--- src/client_src/Animaciones/Animaciones_Objetos/animacion_gema.cpp
+++ src/client_src/Animaciones/Animaciones_Objetos/animacion_gema.cpp
@@ -5,7 +5,7 @@ Animacion_Gema::Animacion_Gema() : Animacion() {}
 void Animacion_Gema::crear_texturas(SDL2pp::Renderer *render) {
 
     this->texturas = std::unique_ptr<SDL2pp::Texture>(new SDL2pp::Texture(this->crear_surface_y_texturas(PATH_GEMA, render)));
-    this->size_frame = 23.75F;
+    this->size_frame = texturas->GetHeight();
     this->cantidad_frames = texturas->GetWidth() / this->size_frame;
 }
 
--- src/client_src/Animaciones/Animaciones_Objetos/animacion_moneda.cpp
+++ src/client_src/Animaciones/Animaciones_Objetos/animacion_moneda.cpp
@@ -5,7 +5,7 @@ Animacion_Moneda::Animacion_Moneda() : Animacion() {}
 void Animacion_Moneda::crear_texturas(SDL2pp::Renderer *render) {
 
     this->texturas = std::unique_ptr<SDL2pp::Texture>(new SDL2pp::Texture(this->crear_surface_y_texturas(PATH_MONEDA, render)));
-    this->size_frame = 16.6F;
+    this->size_frame = texturas->GetHeight();
     this->cantidad_frames = texturas->GetWidth() / this->size_frame;
 }
 
