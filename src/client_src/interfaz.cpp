9dfac22c0b2db4de9109178d4e96ea9d8db292a9
fource <kevincre20@gmail.com>
2024-06-09 22:04:57 -0300

    Agregado top 3 puntaje

--- src/client_src/interfaz.cpp
+++ src/client_src/interfaz.cpp
@@ -65,14 +65,14 @@ void Interfaz::mostrar_hud(SDL2pp::Renderer &render, std::map<uint32_t, std::uni
     rect.w = (ICONO_WIDHT);
     rect.h = (ICONO_HEIGHT);
     render.Copy(* texturas["vida"], SDL2pp::NullOpt, rect);
-    SDL2pp::Surface surface_texto = font.RenderText_Solid(texto, SDL_Color{0, 0, 0, 0}); // Color negro
+    SDL2pp::Surface surface_texto = font.RenderText_Solid(texto, SDL_Color{255, 255, 255, 255}); // Color negro
     SDL2pp::Texture texto_textura(render, surface_texto);
     render.Copy(texto_textura, SDL2pp::NullOpt, SDL2pp::Rect(ICONO_WIDHT+5, alto_ventana-surface_texto.GetHeight()-5, ICONO_WIDHT+10, ICONO_HEIGHT+10));
 
     // Muestro tiempo
 
     texto = std::to_string(tiempo);
-    SDL_Color color = {0, 0, 0, 0};
+    SDL_Color color = {255, 255, 255, 255};
     if (this->tiempo <= 30) { // Ver como terminar el juego...
         color = {255, 0, 0, 0};
     }
@@ -80,17 +80,61 @@ void Interfaz::mostrar_hud(SDL2pp::Renderer &render, std::map<uint32_t, std::uni
     texto_textura = SDL2pp::Texture(render, surface_texto);
     render.Copy(texto_textura, SDL2pp::NullOpt, SDL2pp::Rect(ancho_ventana/2.0, 0, 50, 50));
 
-
+    
     // Muestro puntaje del jugador
-    color = {0, 0, 0, 0};
+    color = {255, 255, 255, 255};
     texto = "x" + std::to_string(personajeViewPtr.obtener_puntos());
     surface_texto = font.RenderText_Solid(texto, color);
     texto_textura = SDL2pp::Texture(render, surface_texto);
     render.Copy(texto_textura, SDL2pp::NullOpt, SDL2pp::Rect(0, 0, 50, 50));
 
     // Muestro los mejores 3 puntajes de los jugadores
+        // Vector para almacenar los puntajes
+    std::vector<std::pair<uint32_t, uint32_t>> puntajes;
 
+        // Recorrer el mapa y extraer los puntajes
+    for (const auto &entry : personajesViews) {
+        uint32_t id = entry.first;
+        uint32_t puntaje = entry.second->obtener_puntos();
+        puntajes.emplace_back(id, puntaje);
+    }
+
+    // Ordenar los puntajes de mayor a menor
+    std::sort(puntajes.begin(), puntajes.end(), [](const auto &a, const auto &b) {
+        return a.second > b.second;
+    });
+
+    // Mostrar los puntajes
+    std::cout << "-------------------------" << std::endl;
+    size_t i = 0;
+    for (const auto &entry : puntajes) {
+        if (i >= 3) {
+            return; // Solo muestro el top 3...
+        }
+        uint32_t id = entry.first;
+        uint32_t puntaje = entry.second;
+        std::cout << "ID: " << id << " - Puntaje: " << puntaje << std::endl;
+        
+        if (entry.first == id_jugador) {
+            color = {0, 255, 0, 0};
+        } else {
+            color = {255, 255, 255, 255};
+        }
+        // Muestro puntaje del jugador
+        std::string texto = std::to_string(id) + ": " + std::to_string(puntaje);
+        SDL2pp::Surface surface_texto = font.RenderText_Solid(texto, color);
+        SDL2pp::Texture texto_textura(render, surface_texto);
+        
+        // Calcular posición de renderizado
+        int x = ancho_ventana - 130; // Ajustar para que no se salga de la ventana
+        int y = i * 50;
+        render.Copy(texto_textura, SDL2pp::NullOpt, SDL2pp::Rect(x, y, 100, 30));
+        
+        i++;
+    }
     // Ver como rellenar el texto de blanco, asi se ve mejor...
+
+    
 }
 
 Interfaz::~Interfaz() {
